78e511b7ab66e07ba334b78b181ef029a6d9e6e7
Cyril Augier <79850947+cyrilaugier@users.noreply.github.com>
2022-07-04 22:35:17 +0200

    Ajoute un modèle de base

--- src/index.php
+++ src/index.php
@@ -1,19 +1,3 @@
 <?php
 
-$config = array(
-    "host" => "localhost",
-    "user" => "alaska",
-    "password" => "password",
-    "database" => "alaska_database",
-);
-
-
-try {
-    $database = new PDO(
-        "mysql:host=".$config["host"].";dbname=".$config["database"].";charset=utf8",
-        $config["user"],
-        $config["password"]
-    );
-} catch (Exception $error) {
-    die("Erreur : " . $error->getMessage());
-}
+require("model.php");
